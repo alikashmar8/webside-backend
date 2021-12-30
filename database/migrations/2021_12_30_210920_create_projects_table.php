09e5c66692dd152784033986ae53a86e4f1dddc5
alikashmar8 <alikashmar8@gmail.com>
2021-12-31 01:46:02 +0200

    Initial

---
+++ database/migrations/2021_12_30_210920_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Enums\ProjectType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('owner')->nullable();
+            $table->string('description');
+            $table->boolean('is_done')->default(true);
+            $table->enum('type', ProjectType::getValues())->default(ProjectType::WEBSITE);
+            $table->string('url');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
